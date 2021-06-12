12ecf203401ba9dce3f21b4e482a97da2e76c03a
noorAbd230 <znoorabd050@gmail.com>
2021-06-12 18:05:49 +0300

    video streaming

--- app/src/main/java/com/example/videostreamingapplication/ButtonActivity.kt
+++ app/src/main/java/com/example/videostreamingapplication/ButtonActivity.kt
@@ -12,25 +12,29 @@ class ButtonActivity : AppCompatActivity() {
 
         videoOne.setOnClickListener {
             var i = Intent(this,MainActivity::class.java)
-            i.putExtra("url","https://firebasestorage.googleapis.com/v0/b/eternal-delight-276016.appspot.com/o/videos%2Fyt1s.com%20-%20Idina%20Menzel%20AURORA%20%20Into%20the%20Unknown%20From%20Frozen%202.mp4?alt=media&token=43edeb22-c358-49e1-ba2a-aaebaed2e6bb")
+            i.putExtra("video","one")
+            i.putExtra("urlOne","https://firebasestorage.googleapis.com/v0/b/eternal-delight-276016.appspot.com/o/videos%2Fyt1s.com%20-%20Idina%20Menzel%20AURORA%20%20Into%20the%20Unknown%20From%20Frozen%202.mp4?alt=media&token=43edeb22-c358-49e1-ba2a-aaebaed2e6bb")
             startActivity(i)
         }
 
         videoTwo.setOnClickListener {
             var i = Intent(this,MainActivity::class.java)
-            i.putExtra("url","https://firebasestorage.googleapis.com/v0/b/eternal-delight-276016.appspot.com/o/videos%2Fyt1s.com%20-%20FROZEN%20%20Let%20It%20Go%20from%20Disneys%20FROZEN%20%20performed%20by%20Idina%20Menzel%20%20Official%20Disney%20UK.mp4?alt=media&token=9388b28a-2257-4089-8814-aa2868b743c1")
+            i.putExtra("video","two")
+            i.putExtra("urlTwo","https://firebasestorage.googleapis.com/v0/b/eternal-delight-276016.appspot.com/o/videos%2Fyt1s.com%20-%20FROZEN%20%20Let%20It%20Go%20from%20Disneys%20FROZEN%20%20performed%20by%20Idina%20Menzel%20%20Official%20Disney%20UK.mp4?alt=media&token=9388b28a-2257-4089-8814-aa2868b743c1")
             startActivity(i)
         }
 
         videoThree.setOnClickListener {
             var i = Intent(this,MainActivity::class.java)
-            i.putExtra("url","https://firebasestorage.googleapis.com/v0/b/eternal-delight-276016.appspot.com/o/videos%2Fyt1s.com%20-%20Do%20You%20Want%20to%20Build%20a%20Snowman%20From%20FrozenSingAlong.mp4?alt=media&token=aee7c1b0-b8e3-44da-b05c-b9c9ba30fc77")
+            i.putExtra("video","three")
+            i.putExtra("urlThree","https://firebasestorage.googleapis.com/v0/b/eternal-delight-276016.appspot.com/o/videos%2Fyt1s.com%20-%20Do%20You%20Want%20to%20Build%20a%20Snowman%20From%20FrozenSingAlong.mp4?alt=media&token=aee7c1b0-b8e3-44da-b05c-b9c9ba30fc77")
             startActivity(i)
         }
 
         videoFour.setOnClickListener {
             var i = Intent(this,MainActivity::class.java)
-            i.putExtra("url","https://www.learningcontainer.com/wp-content/uploads/2020/05/sample-mp4-file.mp4")
+            i.putExtra("video","four")
+            i.putExtra("urlFour","https://www.learningcontainer.com/wp-content/uploads/2020/05/sample-mp4-file.mp4")
             startActivity(i)
         }
     }
--- app/src/main/java/com/example/videostreamingapplication/MainActivity.kt
+++ app/src/main/java/com/example/videostreamingapplication/MainActivity.kt
@@ -3,9 +3,12 @@ package com.example.videostreamingapplication
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.exoplayer2.C
 import com.google.android.exoplayer2.ExoPlayerFactory
 import com.google.android.exoplayer2.SimpleExoPlayer
+import com.google.android.exoplayer2.audio.AudioAttributes
 import com.google.android.exoplayer2.source.ProgressiveMediaSource
+import com.google.android.exoplayer2.ui.AspectRatioFrameLayout
 import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
 import kotlinx.android.synthetic.main.activity_main.*
 import javax.sql.DataSource
@@ -19,7 +22,25 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        url = intent.getStringExtra("url")
+
+        var video = intent.getStringExtra("video")
+        when (video) {
+            "one" -> {
+                url = intent.getStringExtra("urlOne")
+            }
+            "two" -> {
+                url = intent.getStringExtra("urlTwo")
+            }
+            "three" -> {
+                url = intent.getStringExtra("urlThree")
+            }
+            "four" -> {
+                url = intent.getStringExtra("urlFour")
+            }
+            else -> {
+                print("not found")
+            }
+        }
     }
 
     fun initVideo(){
@@ -30,7 +51,14 @@ class MainActivity : AppCompatActivity() {
         var mediaSource = ProgressiveMediaSource.Factory(dataSourceFactory).createMediaSource(uri)
         player!!.playWhenReady = playWhenReady
         player!!.seekTo(currentWindow,playBackPosition)
-        player!!.prepare(mediaSource,false,false)
+        player!!.prepare(mediaSource,false,true)
+        val audioAttributes: AudioAttributes = AudioAttributes.Builder()
+            .setUsage(C.USAGE_MEDIA)
+            .setContentType(C.CONTENT_TYPE_MOVIE)
+            .build()
+
+        player!!.setAudioAttributes(audioAttributes, true)
+
     }
 
     fun releaseVideo(){
@@ -51,6 +79,7 @@ class MainActivity : AppCompatActivity() {
     override fun onPause() {
         super.onPause()
        releaseVideo()
+
     }
 
     override fun onResume() {
@@ -58,8 +87,12 @@ class MainActivity : AppCompatActivity() {
         if (player!=null) initVideo()
     }
 
+
     override fun onStop() {
         super.onStop()
         releaseVideo()
     }
+
+
+
 }
